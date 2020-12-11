a2c2f6d9500b4329de34e82acfbb7da7ee102880
David Ibia <david.ibia@boxmarshall.com>
2020-12-11 05:10:07 +0100

    feat: created resource provider

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const Axios = require('axios').default
+
+const TARGET = 'https://resources.transcendsync.com'
+
+class SyncHandler {
+  /**
+   * Creates a synchronization handler instance
+   * @param {Object} options
+   * @param {String} options.apiKey Transcend Sync Api Key
+   * @param {String} options.siteKey Transcend Sync Site Key
+   */
+  constructor({ apiKey, siteKey }) {
+    this.apiKey = apiKey
+    this.session = Axios.create({
+      baseURL: TARGET,
+      headers: {
+        authorization: `APIKEY ${apiKey}`,
+        sitekey: siteKey
+      }
+    })
+  }
+
+  /**
+   * Fetch deployment configuration
+   */
+  fetchDeployConfig() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const options = {
+          url: 'deploy/config',
+          method: 'POST'
+        }
+
+        const { data } = await this.session(options)
+
+        const { success, resources } = data
+
+        if (!success) {
+          throw new Error('Unable to fetch deploy config')
+        }
+
+        const payloadBuffer = Buffer.from(resources, 'base64')
+        const payloadString = payloadBuffer.toString('utf-8')
+        const payload = JSON.parse(payloadString)
+
+        resolve(payload)
+      } catch (e) {
+        return reject(e)
+      }
+    })
+  }
+}
+
+module.exports = { SyncHandler }
